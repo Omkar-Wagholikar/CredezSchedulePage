ecdcd078e069554b2e5103cd43480205d2aa00f1
Omkar W <omkargwagholikar@gmail.com>
2023-03-26 15:19:07 +0530

    Schedule and Menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 import 'package:laptop/pano.dart';
 import 'FinScheduleScreen/views/ScrollViewSchedule.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,66 +24,131 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SchedulePage extends StatelessWidget {
+class SchedulePage extends StatefulWidget {
+  @override
+  State<SchedulePage> createState() => _SchedulePageState();
+}
+
+class _SchedulePageState extends State<SchedulePage> {
   double ratio = 0.2;
+  int _selectedIndex = 2;
+  static List<Widget> pages = <Widget>[
+    scrollViewTabBar(),
+    Pano(),
+    Pano(),
+    Pano(),
+    Pano(),
+  ];
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Here ya go!",
-          style: TextStyle(fontFamily: 'SolThin'),
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/CalenderBgV3.png"),
+          fit: BoxFit.cover,
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => scrollViewTabBar()),
-                );
-              },
-              child: const Text("Schedules 4.0"),
-            ),
-            ElevatedButton(
-              onPressed: () => showModalBottomSheet(
-                  backgroundColor: Colors.transparent,
-                  isScrollControlled: true,
-                  context: context,
-                  builder: (context) => scrollViewTabBar()),
-              child: const Text("Schedules 5.0"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Pano()),
-                );
-              },
-              child: const Text("Pano re"),
-            ),
-          ],
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text(
+            "Here ya go!",
+            style: TextStyle(fontFamily: 'SolThin'),
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: 'Schedule',
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => scrollViewTabBar()),
+                  );
+                },
+                child: const Text("Schedules 4.0"),
+              ),
+              ElevatedButton(
+                onPressed: () => showModalBottomSheet(
+                    backgroundColor: Colors.transparent,
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (context) => scrollViewTabBar()),
+                child: const Text("Schedules 5.0"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const Pano()),
+                  );
+                },
+                child: const Text("Pano re"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => WheelExample()),
+                  );
+                },
+                child: const Text("Menu re"),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+        ),
+        // body: SafeArea(child: pages[_selectedIndex]),
+        bottomNavigationBar: Container(
+          color: Colors.transparent,
+          child: CurvedNavigationBar(
+            key: _bottomNavigationKey,
+            index: 2,
+            // height: 60.0,
+            height: 50.0,
+            // ignore: prefer_const_literals_to_create_immutables
+            items: <Widget>[
+              Icon(Icons.event,
+                  size: 28,
+                  color: _selectedIndex == 0
+                      ? Color.fromARGB(255, 67, 12, 112)
+                      : Color.fromARGB(255, 202, 138, 255)),
+              Icon(Icons.perm_contact_calendar,
+                  size: 28,
+                  color: _selectedIndex == 1
+                      ? Color.fromARGB(255, 67, 12, 112)
+                      : Color.fromARGB(255, 202, 138, 255)),
+              Icon(Icons.home,
+                  size: 28,
+                  color: _selectedIndex == 2
+                      ? Color.fromARGB(255, 251, 250, 252)
+                      : Color.fromARGB(255, 202, 138, 255)),
+              Icon(Icons.info_outline,
+                  size: 28,
+                  color: _selectedIndex == 3
+                      ? Color.fromARGB(255, 67, 12, 112)
+                      : Color.fromARGB(255, 202, 138, 255)),
+              Icon(Icons.account_circle,
+                  size: 28,
+                  color: _selectedIndex == 4
+                      ? Color.fromARGB(255, 67, 12, 112)
+                      : Color.fromARGB(255, 202, 138, 255)),
+            ],
+            // color: const Color(0xff4d4dff),
+            color: Colors.white.withOpacity(0.2),
+            buttonBackgroundColor: Colors.white.withOpacity(0.2),
+            backgroundColor: Colors.transparent,
+            animationCurve: Curves.easeInOut,
+            animationDuration: const Duration(milliseconds: 400),
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            letIndexChange: (index) => true,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
-          ),
-        ],
-        selectedItemColor: Colors.amber[800],
+        ),
       ),
     );
   }
--- lib/pano.dart
+++ lib/pano.dart
@@ -1,6 +1,9 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:panorama/panorama.dart';
 import 'package:motion_sensors/motion_sensors.dart';
+
+import 'FinScheduleScreen/views/ScrollViewSchedule.dart';
 
 class Pano extends StatefulWidget {
   const Pano({super.key});
@@ -10,35 +13,116 @@ class Pano extends StatefulWidget {
 }
 
 class _PanoState extends State<Pano> {
+  int _selectedIndex = 2;
+  static List<Widget> pages = <Widget>[
+    scrollViewTabBar(),
+    Pano(),
+    Pano(),
+    Pano(),
+    Pano(),
+  ];
+
+  Color changeColor(bool selected) {
+    if (selected) {
+      return Color.fromARGB(255, 255, 255, 255);
+    }
+    return Color.fromARGB(255, 202, 138, 255);
+  }
+
+  final GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       // body: Placeholder(),
-      body: Panorama(
-        sensorControl: SensorControl.Orientation,
-        animSpeed: 1.0,
-        latitude: 90,
-        hotspots: [
-          // Hotspot(
-          //     latitude: 0.0,
-          //     longitude: 0.0,
-          //     width: 90,
-          //     height: 75,
-          //     widget: FloatingActionButton(
-          //       onPressed: () =>
-          //           {print("================WORKS=================")},
-          //       child: Text("1234567890"),
-          //     )),
+      body: Stack(
+        children: [
+          Panorama(
+            // interactive: false,
+            sensorControl: SensorControl.Orientation,
+            animSpeed: 0.1,
+            latitude: 00,
+            longitude: 00,
+            hotspots: const [
+              // Hotspot(
+              // latitude: 0.0,
+              // longitude: 0.0,
+              // width: 90,
+              // height: 75,
+              // widget: FloatingActionButton(
+              //   onPressed: () =>
+              //       {print("================WORKS=================")},
+              //   child: Text("1234567890"),
+              // )),
+            ],
+            // child: const AssetImage("assets/images/CalenderBgV3.png"),
+            // child: Image.asset("assets/images/PanoTest1.jpg"),
+            // child: Image.asset("assets/images/m2.png"),
+            // child: Image.asset("assets/images/swamiKiKripa.png"),
+
+            // child: Image.asset("assets/images/pano6.jpg"),
+            child: Image.asset("assets/images/Sky2.jpeg"),
+
+            // child: Image.asset("assets/images/panoTest2.jpeg"),
+            // child: Image.asset("assets/images/DiscordV2.png"),
+            // child: Image.asset("assets/images/BreadPitV1.png"),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.transparent,
+              child: CurvedNavigationBar(
+                key: _bottomNavigationKey,
+                index: 2,
+                // height: 60.0,
+                height: 50.0,
+                // ignore: prefer_const_literals_to_create_immutables
+                items: <Widget>[
+                  Icon(Icons.event,
+                      size: 28,
+                      color: _selectedIndex == 0
+                          ? changeColor(true)
+                          : changeColor(false)),
+                  Icon(Icons.perm_contact_calendar,
+                      size: 28,
+                      color: _selectedIndex == 1
+                          ? changeColor(true)
+                          : changeColor(false)),
+                  Icon(Icons.home,
+                      size: 28,
+                      color: _selectedIndex == 2
+                          ? changeColor(true)
+                          : changeColor(false)),
+                  Icon(Icons.info_outline,
+                      size: 28,
+                      color: _selectedIndex == 3
+                          ? changeColor(true)
+                          : changeColor(false)),
+                  Icon(Icons.account_circle,
+                      size: 28,
+                      color: _selectedIndex == 4
+                          ? changeColor(true)
+                          : changeColor(false)),
+                ],
+                // color: const Color(0xff4d4dff),
+                color: Colors.white.withOpacity(0.2),
+                buttonBackgroundColor: Colors.white.withOpacity(0.2),
+                backgroundColor: Colors.transparent,
+                animationCurve: Curves.easeInOut,
+                animationDuration: const Duration(milliseconds: 400),
+                onTap: (index) {
+                  setState(() {
+                    _selectedIndex = index;
+                  });
+                },
+                letIndexChange: (index) => true,
+              ),
+            ),
+          ),
         ],
-        // child: const AssetImage("assets/images/CalenderBgV3.png"),
-        // child: Image.asset("assets/images/PanoTest1.jpg"),
-        // child: Image.asset("assets/images/m2.png"),
-        child: Image.asset("assets/images/swamiKiKripa.png"),
-        // child: Image.asset("assets/images/moonless.hdr"),
-        // child: Image.asset("assets/images/panoTest2.jpeg"),
-        // child: Image.asset("assets/images/DiscordV2.png"),
-        // child: Image.asset("assets/images/BreadPitV1.png"),
       ),
+      // bottomNavigationBar:
     );
   }
 }
